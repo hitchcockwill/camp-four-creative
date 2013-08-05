4ecde260c1947fca4e92677d84f3c11694097a88
hitchcockwill <hitchcock.will@gmail.com>
2013-08-05 16:36:21 -0700

    image sizing in header

--- _site/coffee/base.coffee
+++ _site/coffee/base.coffee
@@ -1,1 +1,27 @@
-console.log "hello world"
+
+handleImageSizing = ($image) ->
+
+  setImageSize($image)
+
+  resizing = false
+  $(window).resize ->
+    resizing = true
+
+  setInterval ->
+    if resizing
+      setImageSize($image)
+      resizing = false
+  , 100
+
+setImageSize = ($image) ->
+  if $image.height() < $image.parent().height()
+    $image.parent().css("height", String($image.height()) + "px")
+  else 
+    $image.parent().css("height", "500px")
+
+$(()->
+
+  $imageWrap = $("img.main-image")
+  if $imageWrap.length then handleImageSizing($imageWrap)
+
+)
--- javascripts/base.js
+++ javascripts/base.js
@@ -1,4 +1,35 @@
 (function() {
-  console.log("hello world");
+  var handleImageSizing, setImageSize;
+
+  handleImageSizing = function($image) {
+    var resizing;
+    setImageSize($image);
+    resizing = false;
+    $(window).resize(function() {
+      return resizing = true;
+    });
+    return setInterval(function() {
+      if (resizing) {
+        setImageSize($image);
+        return resizing = false;
+      }
+    }, 100);
+  };
+
+  setImageSize = function($image) {
+    if ($image.height() < $image.parent().height()) {
+      return $image.parent().css("height", String($image.height()) + "px");
+    } else {
+      return $image.parent().css("height", "500px");
+    }
+  };
+
+  $(function() {
+    var $imageWrap;
+    $imageWrap = $("img.main-image");
+    if ($imageWrap.length) {
+      return handleImageSizing($imageWrap);
+    }
+  });
 
 }).call(this);
