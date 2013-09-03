70caa4783580c9326f01cfdb28e68421928895b2
hitchcockwill <hitchcock.will@gmail.com>
2013-09-03 11:11:09 -0700

    added script uglify

--- Gruntfile.js
+++ Gruntfile.js
@@ -31,12 +31,23 @@ module.exports = function(grunt) {
           cssDir: 'css'
         }
       }
+    }, 
+
+    uglify: {
+      production: {
+        files: [
+          {src: ['javascripts/libs/categorizr.js'], dest: 'javascripts/min/lib.min.js'},
+          {src: 'javascripts/*.js', dest: 'javascripts/min/scripts.min.js'}
+        ]
+      }
     }
   }); 
   grunt.loadNpmTasks('grunt-contrib-coffee');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
 
   grunt.registerTask('default', ['watch']);
   grunt.registerTask('compile', ['coffee', 'compass']);
+  grunt.registerTask('production', ['coffee', 'compass', 'uglify']);
 };
